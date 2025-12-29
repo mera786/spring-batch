20be399964e133280cab265b7c417930a7e02eaf
Meraz Alam <merazalam18011998@gmail.com>
2025-12-29 16:33:20 +0530

    Initial commit

---
+++ Spring-batch/src/main/java/com/codesnippet/SpringBatchDemo/entity/Person.java
@@ -0,0 +1,32 @@
+package com.codesnippet.SpringBatchDemo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String phone;
+    private String dateOfBirth;
+    private String jobTitle;
+
+
+}
